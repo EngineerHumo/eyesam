fcf966b3cc6c8bf92c22c39f5c4866b70bbf5311
EngineerHumo <jiaqi.guo@robotrak.cn>
2026-01-22 00:56:05 +0000

    update

--- onnx_produce.py
+++ onnx_produce.py
@@ -171,7 +171,7 @@ class SAM2OnnxWrapper(torch.nn.Module):
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Export SAM2 ONNX model")
-    parser.add_argument("--checkpoint", default="/home/wensheng/gjq_workspace/eyesam/exp_log/checkpoints/best_dice_epoch_0191_second_0p773566_third_0p792149_final_0p900991.pt", help="Path to model weights")
+    parser.add_argument("--checkpoint", default="/home/wensheng/gjq_workspace/eyesam/exp_log_260116_test7_iteration_batch2/checkpoints/best_dice_epoch_0169_second_0p788646_third_0p870307_final_0p932746.pt", help="Path to model weights")
     parser.add_argument(
         "--config",
         default="sam2/configs/sam2.1_hiera_tiny512_laser.yaml",
@@ -179,7 +179,7 @@ def main() -> None:
     )
     parser.add_argument(
         "--output",
-        default="onnx/iteration_191.onnx",
+        default="onnx/iteration_v2_169.onnx",
         help="Output ONNX path",
     )
     parser.add_argument("--device", default="cpu", help="Device for export")
